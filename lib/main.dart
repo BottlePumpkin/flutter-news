e3ff870fa6aa08471a5d38b4deb1d921edcb100b
DustinPark92 <61003485+DustinPark92@users.noreply.github.com>
2022-02-19 15:35:19 +0900

    * ✨feature: mainDropDownButton 컴포넌트로 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news/app/config/app_provider_config.dart';
 import 'package:flutter_news/app/palette/network/http_client_impl.dart';
 import 'package:flutter_news/app/palette/network/uri_extension.dart';
 import 'package:flutter_news/data/api/story_api.dart';
 import 'package:flutter_news/presentation/story/story_main/story_main_view.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,12 +16,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(
+      providers: AppProviderConfig.providers(),
+      child: MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: StoryMainView(),
-    );
+    ),);
   }
 }
--- lib/presentation/story/story_main/story_main_view.dart
+++ lib/presentation/story/story_main/story_main_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news/presentation/story/story_main/components/main_dropdown_button.dart';
 import 'package:flutter_news/presentation/story/story_main/story_main_view_model.dart';
 import 'package:provider/provider.dart';
 
@@ -8,50 +9,44 @@ class StoryMainView extends StatefulWidget {
 }
 
 class _StoryMainViewState extends State<StoryMainView> {
-
-
   @override
   void initState() {
-    Provider.of<StoryMainViewModel>(context,listen: false).fetchStoriesList();
+    Provider.of<StoryMainViewModel>(context, listen: false).fetchStoriesList();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-
-
     StoryMainViewModel _viewModel = Provider.of<StoryMainViewModel>(context);
 
     return Scaffold(
       appBar: AppBar(
-        title: DropdownButton(
-          value: _viewModel.currentCategoryValue,
-          items: _viewModel.storyCategory
-              .map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
-          onChanged: (String? value) {
-            _viewModel.dropDownMenuName(value!);
-          },
-        ),
+        title: MainDropdownButton(viewModel: _viewModel),
       ),
       body: RefreshIndicator(
         onRefresh: () async {
           _viewModel.fetchStoriesList();
         },
-        child:
+        child: !_viewModel.isLoading
+            ? Container(
+                color: Colors.red,
+              )
+            : ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () {
 
-        !_viewModel.isLoading ? Container(color: Colors.red,) :
-        ListView.builder(
-          itemBuilder: (BuildContext context, int index) {
-            return GestureDetector(
-                child: Container(
-                    height: 80, child: Text(_viewModel.storyList[index])));
-          },
-        ),
+                    },
+                    child: Container(
+                      height: 80,
+                      child: Text(
+                        _viewModel.storyList[index],
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                  );
+                },
+              ),
       ),
     );
   }
