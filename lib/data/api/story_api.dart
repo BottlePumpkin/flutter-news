e487fa132b9496a364681e518800b732ad635f90
DustinPark92 <61003485+DustinPark92@users.noreply.github.com>
2022-02-19 13:41:56 +0900

    * ♻️refactor: news -> story로 파일 및 폴더 이름 변경

--- lib/data/api/story_api.dart
+++ lib/data/api/story_api.dart
@@ -1,12 +1,18 @@
 
-
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_news/app/palette/network/http_client_impl.dart';
+import 'package:flutter_news/app/palette/network/uri_extension.dart';
 
 class StoryApi extends ChangeNotifier {
-  Future<List<int>> fetchStoriesList() async {
+  Future<List<dynamic>> fetchStoriesList() async {
+    Map<String,String> parms = {
+      'print' : 'pretty'
+    };
+    Uri uri = UriExtension.fetchUri(url: 'topstories',queryParams: parms);
+    var _result = await HttpClientImpl().get(uri: uri);
+    List<dynamic> response = _result.map((e) => e.toString()).toList();
 
-
+    return response;
   }
 
 
