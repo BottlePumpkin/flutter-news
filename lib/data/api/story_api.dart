319829a059cda49d244e6001b116d84b5458954d
DustinPark92 <61003485+DustinPark92@users.noreply.github.com>
2022-02-19 14:32:58 +0900

    ✨feature: story_api에 fetchStoryDetail api추가

--- lib/data/api/story_api.dart
+++ lib/data/api/story_api.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_news/app/palette/network/http_client_impl.dart';
 import 'package:flutter_news/app/palette/network/uri_extension.dart';
+import 'package:flutter_news/data/model/story_detail_model.dart';
 
 class StoryApi extends ChangeNotifier {
   Future<List<dynamic>> fetchStoriesList() async {
@@ -11,6 +12,16 @@ class StoryApi extends ChangeNotifier {
     Uri uri = UriExtension.fetchUri(url: 'topstories',queryParams: parms);
     var _result = await HttpClientImpl().get(uri: uri);
     return _result;
+  }
+
+  Future<StoryDetailModel> fetchStoryDetail() async {
+    Map<String,String> parms = {
+      'print' : 'pretty'
+    };
+    Uri uri = UriExtension.fetchUri(url: 'item/30392227',queryParams: parms);
+    var _result = await HttpClientImpl().get(uri: uri);
+    final value = StoryDetailModel.fromJson(_result);
+    return value;
   }
 
 
