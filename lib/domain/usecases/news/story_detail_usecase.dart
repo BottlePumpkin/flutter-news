b2bf3cf08a7ef267747c68b4bf87b0a21dc143bf
DustinPark92 <61003485+DustinPark92@users.noreply.github.com>
2022-02-19 16:04:44 +0900

    * ✨feature: story_detail_entity, story_detail_usecase.dart 추가

---
+++ lib/domain/usecases/news/story_detail_usecase.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_news/data/repositories/story_repository.dart';
+import 'package:flutter_news/domain/entities/news/story_detail_entity.dart';
+
+class StoryDetailUsecase {
+  final StoryRepository repository;
+
+  StoryDetailUsecase({required this.repository});
+
+  Future<StoryDetailEntity> call(String id) async {
+    final response = await repository.fetchStoryDetail(id);
+
+    return StoryDetailEntity(
+              by: response.by,
+              descendants: response.descendants,
+              id: response.id,
+              kids: response.kids,
+              score: response.score,
+              time: response.time,
+              title: response.title,
+              type: response.type,
+              url: response.url);
+  }
+}
