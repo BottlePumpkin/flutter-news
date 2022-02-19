c3b10de14f46fbd101c7568e19f458988e647e2e
DustinPark92 <61003485+DustinPark92@users.noreply.github.com>
2022-02-19 15:29:44 +0900

    ✨feature: story_main_view 화면 구성 및 data fetching

--- lib/presentation/story/story_main/story_main_view.dart
+++ lib/presentation/story/story_main/story_main_view.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news/presentation/story/story_main/story_main_view_model.dart';
+import 'package:provider/provider.dart';
 
-class StoryMainView extends StatelessWidget {
-  const StoryMainView({Key? key}) : super(key: key);
+class StoryMainView extends StatefulWidget {
+  @override
+  State<StoryMainView> createState() => _StoryMainViewState();
+}
+
+class _StoryMainViewState extends State<StoryMainView> {
+
+
+  @override
+  void initState() {
+    Provider.of<StoryMainViewModel>(context,listen: false).fetchStoriesList();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+
+    StoryMainViewModel _viewModel = Provider.of<StoryMainViewModel>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: DropdownButton(
+          value: _viewModel.currentCategoryValue,
+          items: _viewModel.storyCategory
+              .map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+          onChanged: (String? value) {
+            _viewModel.dropDownMenuName(value!);
+          },
+        ),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          _viewModel.fetchStoriesList();
+        },
+        child:
+
+        !_viewModel.isLoading ? Container(color: Colors.red,) :
+        ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+                child: Container(
+                    height: 80, child: Text(_viewModel.storyList[index])));
+          },
+        ),
+      ),
+    );
   }
 }
